1237f227a8673e97b94fb8d30479bb8a0ec543af
Guy Brukhis <guy.br7@gmail.com>
2017-03-20 23:59:32 +0200

    add filter icon

--- public/app.js
+++ public/app.js
@@ -37,7 +37,10 @@ Vue.component('comments-feed', {
   props: ['comments', 'is_fetching'],
   template: `
     <div class="comments-feed">
-      <input v-model="filter" type="text" placeholder="Filter">
+      <div class="filter-container">
+        <search-icon></search-icon>
+        <input v-model="filter" type="text" placeholder="Filter">
+      </div>
       <comments-item v-for="comment in filtered_comments" :email="comment.email" :message="comment.message" :key="comment.id"></comments-item>
       <spinner v-if="is_fetching"></spinner>
       <p v-else-if="empty_comments" class="empty-comments">No comments yet!</p>
@@ -97,6 +100,22 @@ Vue.component('spinner', {
       <div class="bounce1"></div>
       <div class="bounce2"></div>
       <div class="bounce3"></div>
+    </div>
+  `
+})
+
+Vue.component('search-icon', {
+  template: `
+    <div class="search-icon">
+      <?xml version="1.0" encoding="iso-8859-1"?>
+      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve">
+        <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23
+        s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92
+        c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17
+        s-17-7.626-17-17S14.61,6,23.984,6z"/>
+      </svg>
+
     </div>
   `
 })
